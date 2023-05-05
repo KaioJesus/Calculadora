46f49e84d67a635e68d5edbe095e4061b67c4104
Kaio Jesus <kaioramos990@hotmail.com>
2023-05-04 23:54:39 -0300

    inicio js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const operacaoAnterior = document.querySelector('.operacao-anterior');
+const operacaoAtual = document.querySelector('.operacao-atual');
+
+const botoes = document.querySelectorAll('.botoes');
+
+botoes.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+    })
+    console.log(btn);
+})
+
